7c15fadbdee59c22015b1324fdf1a4a0aa4c7547
EC2 Default User <ec2-user@ip-172-31-45-65.ca-central-1.compute.internal>
2021-02-25 16:16:31 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+
+        name='sm-xgboost',
+
+        version='0.0.1',
+
+        description='',
+
+        url='',
+
+        install_requires=[
+
+            'xgboost',
+
+            'imblearn',
+
+            'sagemaker',
+
+        ],
+
+    )
